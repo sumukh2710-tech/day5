32cef4938c074d6aa815f5056e44bd03fb8fae63
sumukh2710-tech <sumukh2710@gmail.com>
2026-01-31 15:05:56 +0530

    Add files via upload

---
+++ code18.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class code18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int smallest = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        System.out.println("Smallest element in the array: " + smallest);
+        sc.close();
+    }
+}
